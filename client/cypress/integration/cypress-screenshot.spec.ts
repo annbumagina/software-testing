9becd4eb0def6fecff6c14ca21872a0b9791d1af
annbumagina <annbumagina@ya.ru>
2021-02-26 17:33:00 +0300

    screenshot

--- client/cypress/integration/cypress-screenshot.spec.ts
+++ client/cypress/integration/cypress-screenshot.spec.ts
@@ -1,11 +1,12 @@
 const URL = "http://localhost:3000";
 
 describe("registering", () => {
-    it("register twice", () => {
+    it("register", () => {
         const login = Math.random().toString(20).substr(2, 10);
         const pass = Math.random().toString(20).substr(2, 10);
 
         cy.visit(URL + '/register');
+        cy.screenshot();
         cy.get("#first").type('Ann');
         cy.get("#last").type('Bumagina');
         cy.get('#login').type(login);
@@ -13,14 +14,6 @@ describe("registering", () => {
         cy.get('.submitButton').click();
         cy.wait(300);
         cy.contains('Successfully registered, you can log in now');
-
-        cy.visit(URL + '/register');
-        cy.get("#first").type('Nna');
-        cy.get("#last").type('Anigabum');
-        cy.get('#login').type(login);
-        cy.get('#pass').type(pass);
-        cy.get('.submitButton').click();
-        cy.wait(300);
         cy.screenshot()
     });
 });
